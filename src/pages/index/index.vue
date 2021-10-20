d4a1187bd184fd954864ca04cf0d72adea0bc30c
vijay <vijayniubi@gmail.com>
2021-10-20 16:58:23 +0800

    1.3.4

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -5,27 +5,13 @@
 let wv;
 export default {
 	onNavigationBarButtonTap(e) {
-		// const currentWebview = this.$scope.$getAppWebview(); //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效
-		// const wv = currentWebview.children()[0]
 		wv.back();
-		// wv.overrideUrlLoading(function() {
-		// 	console.log("777");
-		// })
 	},
 	onReady() {
 		const currentWebview = this.$scope.$getAppWebview(); //此对象相当于html5plus里的plus.webview.currentWebview()。在uni-app里vue页面直接使用plus.webview.currentWebview()无效
-
-		const titleNView = currentWebview.getStyle().titleNView;
 		setTimeout(function() {
 			wv = currentWebview.children()[0];
-			setInterval(() => {
-				wv.canBack(({ canBack }) => {
-					titleNView.buttons[0].type = canBack?'back':'none';
-					currentWebview.setStyle({ titleNView:titleNView });
-				});
-			}, 100);
-		}, 1000);
-
+		}, 700);
 	}
 };
 </script>
